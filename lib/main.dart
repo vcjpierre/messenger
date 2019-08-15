5a3c4f8dd5d1f54169e1d0e388cf3e7991f9d89e
Jean Pierre <vcjpierre@gmail.com>
2019-08-15 23:39:39 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
- 
-void main() => runApp(MyApp());
- 
-class MyApp extends StatelessWidget {
+import 'pages/ConversationPageList.dart';
+
+void main() => runApp(Messio());
+
+class Messio extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+      title: 'Flutter Messenger',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: ConversationPageList(),
     );
   }
 }
