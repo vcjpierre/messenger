59bf54c352add13534bdf870318ba61aa6e7fd40
Jean Pierre <vcjpierre@gmail.com>
2019-09-22 00:19:32 -0400

    Implementing pagination on Chat Screen

--- lib/repositories/ChatRepository.dart
+++ lib/repositories/ChatRepository.dart
@@ -4,11 +4,24 @@ import 'package:flutter_messenger/models/User.dart';
 import 'package:flutter_messenger/providers/BaseProviders.dart';
 import 'package:flutter_messenger/providers/ChatProvider.dart';
 
-class ChatRepository{
+class ChatRepository {
   BaseChatProvider chatProvider = ChatProvider();
+
   Stream<List<Chat>> getChats() => chatProvider.getChats();
-  Stream<List<Message>> getMessages(String chatId) => chatProvider.getMessages(chatId);
-  Future<void> sendMessage(String chatId, Message message) => chatProvider.sendMessage(chatId, message);
-  Future<String> getChatIdByUsername(String username) => chatProvider.getChatIdByUsername(username);
-  Future<void> createChatIdForContact(User user) => chatProvider.createChatIdForContact(user);
+
+  Stream<List<Message>> getMessages(String chatId) =>
+      chatProvider.getMessages(chatId);
+
+  Future<List<Message>> getPreviousMessages(
+          String chatId, Message prevMessage) =>
+      chatProvider.getPreviousMessages(chatId, prevMessage);
+
+  Future<void> sendMessage(String chatId, Message message) =>
+      chatProvider.sendMessage(chatId, message);
+
+  Future<String> getChatIdByUsername(String username) =>
+      chatProvider.getChatIdByUsername(username);
+
+  Future<void> createChatIdForContact(User user) =>
+      chatProvider.createChatIdForContact(user);
 }
--- lib/utils/Exceptions.dart
+++ lib/utils/Exceptions.dart
@@ -1,17 +1,17 @@
-abstract class MessengerException implements Exception{
+abstract class MessengerException implements Exception {
     String errorMessage();
 }
-class UserNotFoundException extends MessengerException{
+class UserNotFoundException extends MessengerException {
   @override
   String errorMessage() => 'No user found for provided uid/username';
 
 }
-class UsernameMappingUndefinedException extends MessengerException{
+class UsernameMappingUndefinedException extends MessengerException {
   @override
   String errorMessage() =>'User not found';
 
 }
-class ContactAlreadyExistsException extends MessengerException{
+class ContactAlreadyExistsException extends MessengerException {
   @override
   String errorMessage() => 'Contact already exists!';
 }
